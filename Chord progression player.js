bc3471015c891dd0d1835d3fc6d7ff33027909db
Anastasia Dunbar <anastasiajdunbar@gmail.com>
2020-10-19 09:53:42 +0200

    Update Chord progression player.js

--- Chord progression player.js
+++ Chord progression player.js
@@ -1,56 +1,82 @@
 importScripts("https://gist.githack.com/AnastasiaDunbar/d75e19d93a5c439d8185343f1fd54da1/raw/b3f445b7cbdd083af8d9743c5b2550c8b1987061/TinyRave%20functions.js");
 var strMatch=(s,r)=>(s.match(r)||[""])[0],
+    objectMap=(o,f)=>Object.fromEntries(Object.entries(o).map(([k,v],i)=>[k,f(v,k,i)])),
     chordProgression=
     "4Abmaj7 4Bb7/Ab 4Gm7b5 4C7 4Fm7 4Ab/Eb 2D7 2D7/F# Gm7 Gbm7 Fm7 Bb7" //“Ah-Choo”
+    //"4F 4F7/D# 4Dm7 2Aaug/C# 2F/C 2Bdim 2A# 7Dm Dm/E 4F6 4F7/Eb 4Dm9 2A7/Db 2Fmaj7/C 2Dm6/9/B 2Fmaj13/Bb 7Dmadd2 Dm/E" //“Im Nin'Alu”
     //"4Dbmaj7 4C7 2Fm 2Em7 2Ebm7 2Ab7 4Bbm7 4Eb7 4Abmaj7 4Fm7 3Fm6 Fm7/Ab 2G7 2Fdim7/B 2Cm7 2Bm7 2Bbm7 2Eb 4Gm 4C 4Fm7 2Ebm7 2Ab7/C"
     //"Bmaj7 D7 Gmaj7 Bb13 2Ebmaj7 Am9 D7 Gmaj9 Bb13 Ebmaj7 F#7 2Bmaj7 Fm7 Bb7 2Ebmaj7 Am7 D7 2Gmaj9 C#m7 F#13 2Bmaj7 Fm9 Bb13 2Ebmaj7 C#madd11 F#7" //“Giant Steps”
     //"3Amaj7/F# 5Dmaj7/B 3Dm9 5A#maj7 3D#maj7/C 5G#maj7/F 3Abm9/G# 5Amaj9/C#" //Something I made.
-    .split(" ").map(
+    //"4Fmaj7 4Am7 4Dm7 4F7 4Bbmaj7 4Am7 4Gm7 4C7"
+    .split(/\s+/g).map(
 	s=>{
 		var object={},t=strMatch(s,/^\d+/);object.length=+t||1;
 		s=s.slice(t.length);
 		t=strMatch(s,/^[A-G][b#]?/);object.note=t;
 		s=s.slice(t.length);
-		t=strMatch(s,/^[a-z0-9#]+/);object.chord=t;
+		t=strMatch(s,/^([a-z0-9#]|\/[0-9])+/);object.chord=t;
 		s=s.slice(t.length);
 		if(/^\/[A-G][b#]?$/.test(s)){
 			object.slash=strMatch(s,/[A-G][b#]?$/);
 		}
 		return object;
 	}
-),chords={
-	"":      [0,4,7], //major
-	m:       [0,3,7], //minor
-	aug:     [0,4,8],
-	dim:     [0,3,6],
-	sus2:    [0,2,7],
-	sus4:    [0,5,7],
-	5:       [0,7],
-	6:       [0,4,7,9],
-	maj7:    [0,4,7,11],
-	m6:      [0,3,7,9],
-	m7:      [0,3,7,10],
-	7:       [0,4,7,10], //dom7
-	madd11:  [0,3,5,7],
-	m7add11: [0,3,7,10,17],
-	maj7b5:  [0,4,6,11],
-	m7b5:    [0,3,6,10],
-	dim7:    [0,3,6,9],
-	maj9:    [0,4,7,11,14],
-	m9:      [0,3,7,10,14],
-	9:       [0,4,7,10,14],
-	13:      [0,4,7,10,14,21],
-	maj7sus2:[0,2,7,11],
-	"7sus4": [0,5,7,10]
-};
+),chords=objectMap({
+	"":[4,7], //major
+	m:[3,7], //minor
+	aug:[4,8],
+	augsus4:[5,8],
+	b5:[4,6],
+	dim:[3,6],mb5:[3,6],
+	sus2:[2,7],
+	sus4:[5,7],
+	sus24:[2,5,7],
+	add2:[2,4,7],add9:[4,7,14],
+	add4:[4,5,7],add11:[4,7,17],
+	madd2:[2,3,7],madd9:[3,7,14],
+	madd4:[3,5,7],madd11:[3,7,17],
+	5:[7],
+	6:[4,7,9],
+	m6:[3,7,9],
+	m69:[3,7,9,14],"m6/9":[3,7,9,14],
+	69:[4,7,9,14],"6/9":[4,7,9,14],
+	7:[4,7,10],dom7:[4,7,10],
+	maj7:[4,7,11],
+	m7:[3,7,10],
+	m7add11:[3,7,10,17],
+	maj7b5:[4,6,11],
+	m7b5:[3,6,10], //half-diminished
+	m7b9:[1,3,7,10],
+	m7b13:[3,7,8,10],
+	dim7:[3,6,9],
+	maj7sus2:[2,7,11],
+	maj7sus4:[5,7,11],
+	"maj7#11":[4,7,11,18],
+	"7sus2":[2,7,10],
+	"7sus4":[5,7,10],
+	mmaj7:[3,7,11],"m#7":[3,7,11],
+	"7#5":[4,8,10],"9#5":[4,8,14],
+	"7b9":[4,7,10,13],
+	"7#9":[4,7,10,15],
+	9:[4,7,10,14],
+	maj9:[4,7,11,14],
+	m9:[3,7,10,14],
+	"9sus4":[5,7,10,14],
+	"9b5":[4,6,10,14],
+	11:[4,7,10,14,17],
+	m11:[3,7,10,14,17],
+	13:[4,7,10,14,21],
+	maj13:[7,11,14,16,21]
+},x=>[0].concat(x));
 function findChords(notes){
 	if(!(Array.isArray(notes)&&notes.length>0)){return[];}
 	var minimum=Math.min(...notes),out=[];
 	notes=notes.map(x=>(x-minimum)%12);
 	for(var key in chords){
 		var chord=chords[key].map(x=>x%12);
+		if(notes.length!==chord.length){continue;}
 		for(var i=0;i<chord.length;i++){
-			if(notes.every(x=>chord.includes(x))&&notes.length===chord.length){
+			if(notes.every(x=>chord.includes(x))){
 				out.push(key);break;
 			}
 			chord=chord.map((x,i)=>mod(x-chord[1],12)); //The inversion could give the same notes.
